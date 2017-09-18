36eda50a2e89bd6a989109c14f568fb05feb0927
nunezrip <nunezrip@aol.com>
2017-09-18 17:38:58 -0400

    Add files via upload

---
+++ 1.5_From_a_While_Loop_to_a_For_Loop.js
@@ -0,0 +1,12 @@
+// Video 1.4 accompanies this exercise
+
+// Write a basic for loop that prints to the console all
+// the numbers from 10 to 1 in descending order. This is the similar to one
+// of the previous challenges, but this time we’re using a for loop instead
+// of a while loop.
+
+
+
+for(let i = 10; i > 0; i = i - 1){
+    console.log(`The loop has run ${i} times.`)
+}
---
+++ 4.2_A_Basic_Multiplication_Function.js
@@ -0,0 +1,16 @@
+// Video 4.1 accompanies this exercise
+
+// Build a function declaration called multiplyTrio that takes in three
+// parameters. Inside the function, multiply the numbers together into one
+// product, and return the result. You may use whatever parameter and variable
+// names you’d like.
+
+
+
+function multiplyTrio (num1, num2, num3){
+   const multiplyTrio = num1 * num2 * num3
+    return `product is: ${multiplyTrio}`
+
+}
+
+console.log(multiplyTrio (4, 5, 10));
